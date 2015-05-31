7340bddac2d57e0aab32927ddb6b45c006246da9
Benjamin Falk <bn_falk@yahoo.com>
2015-05-31 11:37:36 -0500

    Adding a touch of science

--- lib/pattern_performance.ex
+++ lib/pattern_performance.ex
@@ -1,4 +1,8 @@
 defmodule PatternPerformance do
+  defstruct slowest_time: 0,
+            total_time: 0,
+            words_tried: 0
+
   def words do
     File.read!(Path.join([__DIR__, "../words.txt"]))
       |> String.split("\n")
@@ -7,5 +11,22 @@ defmodule PatternPerformance do
   def record_access(module) do
     # "Warming" up the system
     module.is_word?("apple")
+    Enum.reduce(words, %PatternPerformance{}, fn word, performance ->
+      time =
+        case :timer.tc(module, :is_word?, [word]) do
+          {t, true} -> t
+          {t, false} ->
+            IO.puts "Word '#{word}' not found!"
+            t
+        end
+      %{performance |
+        slowest_time: slowest_time(time, performance.slowest_time),
+        words_tried: performance.words_tried + 1,
+        total_time: performance.total_time + time
+      }
+    end)
   end
+
+  defp slowest_time(time, slowest_time) when time > slowest_time, do: time
+  defp slowest_time(_, slowest_time), do: slowest_time
 end
